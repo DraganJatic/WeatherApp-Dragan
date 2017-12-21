d6de66b7100bc884e381d5cfa4253c48e8af2b0a
DraganJatic <skippadaflippayrn55@gmail.com>
2017-12-21 13:25:17 +0100

    Initial commit

---
+++ app/src/main/java/rs/edu/educons/finteh/pma/weatherapp/MainActivity.java
@@ -0,0 +1,13 @@
+package rs.edu.educons.finteh.pma.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
